fc86d92b2633481486fd13a0d28b3c4b8df42d3f
Ayush Gupta <721ayush@gmail.com>
2020-07-28 12:55:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+
+ const express = require('express');
+ const app = express()
+ const port = 8000
+
+
+
+
+ app.listen(port, function(err){
+     if(err){
+         console.log(`Error in running the server : ${err}`);
+     }
+     console.log(`Server is running on port: ${port}`)
+ })
