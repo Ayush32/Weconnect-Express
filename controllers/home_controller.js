9034101967524d08ff16551362bc197f3e38a05f
Ayush Gupta <721ayush@gmail.com>
2020-07-28 20:38:21 +0530

    create first view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,9 @@
 /*
- *   Copyright (c) 2020 
+ *   Copyright (c) 2020
  *   All rights reserved.
  */
-module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Weconnect express</h1>')
-}
-
+module.exports.home = function (req, res) {
+  return res.render("home", {
+    titleName: "Weconnect Home",
+  });
+};
