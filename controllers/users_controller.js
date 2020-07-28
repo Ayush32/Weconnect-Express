57569d015f86dd86add711f922337ca835fa7f35
Ayush Gupta <721ayush@gmail.com>
2020-07-28 20:59:15 +0530

    implementing patials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,9 @@
 /*
- *   Copyright (c) 2020 
+ *   Copyright (c) 2020
  *   All rights reserved.
  */
 module.exports.profile = function (rex, res) {
-  return res.end("<h1>user profile</h1>");
+  return res.render("user_profile", {
+    titleName: "User Profile",
+  });
 };
