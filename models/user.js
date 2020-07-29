ac42866d9a75c248a7e8841b5d32355070d1cd8f
Ayush Gupta <721ayush@gmail.com>
2020-07-29 19:32:53 +0530

    schema is for user

---
+++ models/user.js
@@ -0,0 +1,21 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
