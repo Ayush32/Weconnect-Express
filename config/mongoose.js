0c6cbadd64035b6bff94b35a74ceec8e4fb360a5
Ayush Gupta <721ayush@gmail.com>
2020-07-29 19:18:09 +0530

    linking to mongod with mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/weconnect_express_development");
+
+const db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "Error connecting to Mongodb"));
+
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
+});
+
+module.exports = db;
