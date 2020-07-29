3ccb0ea22a90082d924aaeac57088f443b3f9b29
Ayush Gupta <721ayush@gmail.com>
2020-07-29 20:28:38 +0530

    creating and altering a cookie

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,9 @@
  *   All rights reserved.
  */
 module.exports.home = function (req, res) {
+  console.log(req.cookies);
+  res.cookie("user_id", 399);
+
   return res.render("home", {
     titleName: "Hiveconnect Home",
   });
