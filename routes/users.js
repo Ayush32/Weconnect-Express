0f8803495d47f9db4f3a4ec31ef46c430f6e42b3
Ayush Gupta <721ayush@gmail.com>
2020-07-29 20:46:37 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ router.get("/profile", usersController.profile);
 router.get("/sign-up", usersController.signUp);
 router.get("/sign-in", usersController.signIn);
 
+// create the session
 router.post("/create", usersController.create);
 
 module.exports = router;
