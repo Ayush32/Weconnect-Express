014e2649c957d6b0896330d91ef91b3fd85cf3e2
Ayush Gupta <721ayush@gmail.com>
2020-07-29 20:38:20 +0530

    create sign up page auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,6 +3,7 @@
  *   All rights reserved.
  */
 const { model } = require("../models/user");
+const User = require("../models/user");
 
 /*
  *   Copyright (c) 2020
@@ -30,7 +31,27 @@ module.exports.signIn = function (req, res) {
 // get the sign data
 
 module.exports.create = function (req, res) {
-  // TODO
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Error in finding user in signing up");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error in creating user while in signing up");
+          return;
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
 
 module.exports.createSession = function (req, res) {
