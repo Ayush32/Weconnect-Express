126058a85397ead10188e89404171185245d3ddf
Ayush Gupta <721ayush@gmail.com>
2020-07-29 19:54:42 +0530

    rendering pages for signup and signIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const { model } = require("../models/user");
+
 /*
  *   Copyright (c) 2020
  *   All rights reserved.
@@ -5,5 +7,18 @@
 module.exports.profile = function (rex, res) {
   return res.render("user_profile", {
     titleName: "User Profile",
+  });
+};
+
+// render signup page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    titleName: "WeConnect | Sign Up",
+  });
+};
+// render signIn page
+module.exports.signIn = function (req, res) {
+  return res.render("user_Sign-in", {
+    titleName: "WeConnect | Sign In",
   });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 /*
- *   Copyright (c) 2020 
+ *   Copyright (c) 2020
  *   All rights reserved.
  */
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/users_controller')
+const usersController = require("../controllers/users_controller");
 
-router.get('/profile', usersController.profile)
-
-
+router.get("/profile", usersController.profile);
+router.get("/sign-up", usersController.signUp);
+router.get("/sign-in", usersController.signIn);
 
 module.exports = router;
