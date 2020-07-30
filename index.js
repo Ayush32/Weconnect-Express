bc26f099e17d7d88f83ca833dd971f154e74fbb2
Ayush Gupta <721ayush@gmail.com>
2020-07-31 00:10:48 +0530

    install express-session

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const cookieParser = require("cookie-parser");
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+// used for session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 app.use(express.urlencoded());
 
@@ -18,6 +22,21 @@ app.use(express.static("./assets"));
 app.use(expressLayouts);
 
 // extract style  and scripts from sub pages into the layout
+app.use(
+  session({
+    name: "hiveconnect",
+    // TODO change the secret before deployment
+    secret: "bellaciao",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
