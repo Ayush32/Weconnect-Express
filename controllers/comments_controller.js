ac3aeb47278214de7f492ea33111d652cd7c5286
Ayush Gupta <721ayush@gmail.com>
2020-08-05 12:41:31 +0530

    add comments to the db

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -6,4 +6,22 @@ const Post = require("../models/post");
 const User = require("../models/user");
 const Comment = require("../models/comment");
 
-module.exports.create = function (req, res) {};
+module.exports.create = function (req, res) {
+  Post.findById(req.body.post, function (err, post) {
+    if (post) {
+      Comment.create(
+        {
+          content: req.body.content,
+          post: req.body.post,
+          user: req.user._id,
+        },
+        function (err, comment) {
+          post.comments.push(comment);
+          post.save();
+
+          res.redirect("/");
+        }
+      );
+    }
+  });
+};
