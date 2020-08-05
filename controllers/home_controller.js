859f8eafff21c51c39c528c2fe4afb78ed87208a
Ayush Gupta <721ayush@gmail.com>
2020-08-05 11:02:12 +0530

    display user name on posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,13 +3,21 @@
  *   All rights reserved.
  */
 
-const Post = require('../models/post')
+const Post = require("../models/post");
 module.exports.home = function (req, res) {
+  // Post.find({},function(err, posts){
+  //   return res.render('home', {
+  //     titleName: "Sociolbox | Home",
+  //     posts: posts
+  //   });
+  // });
 
-  Post.find({},function(err, posts){
-    return res.render('home', {
-      titleName: "Sociolbox | Home",
-      posts: posts
-    })
-  })
-}
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        titleName: "Sociolbox | Home",
+        posts: posts,
+      });
+    });
+};
