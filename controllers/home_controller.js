fea864ad8abdc0202609c565fa3de93c822e4be9
Ayush Gupta <721ayush@gmail.com>
2020-08-05 10:46:51 +0530

    display posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,8 +2,14 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
+
+const Post = require('../models/post')
 module.exports.home = function (req, res) {
-  return res.render("home", {
-    titleName: "Hiveconnect Home",
-  });
-};
+
+  Post.find({},function(err, posts){
+    return res.render('home', {
+      titleName: "Sociolbox | Home",
+      posts: posts
+    })
+  })
+}
