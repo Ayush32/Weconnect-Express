5c9634907dbd03c58c42e51df9d4a044d7d6046e
Ayush Gupta <721ayush@gmail.com>
2020-08-05 12:28:36 +0530

    create comment schema

---
+++ controllers/comments_controller.js
@@ -0,0 +1,9 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+const Post = require("../models/post");
+const User = require("../models/user");
+const Comment = require("../models/comment");
+
+module.exports.create = function (req, res) {};
