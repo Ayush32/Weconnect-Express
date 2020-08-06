9d1fdbba6752692f6bf5f5268571d4be8f158d0a
Ayush Gupta <721ayush@gmail.com>
2020-08-06 11:54:51 +0530

    deleteing comments

--- routes/comments.js
+++ routes/comments.js
@@ -7,7 +7,15 @@ const router = express.Router();
 const passport = require("passport");
 
 const commentsController = require("../controllers/comments_controller");
+const { route } = require("./posts");
+const { destroy } = require("../controllers/posts_controller");
 
 router.post("/create", passport.checkAuthentication, commentsController.create);
+
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  commentsController.destroy
+);
 
 module.exports = router;
